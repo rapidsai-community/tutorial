fc8d100d8dcee4aacf044ac6549040a1a0a961a7
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2026-07-02 11:19:19 -0700

    Add SPDX headers and update license

--- data_setup.py
+++ data_setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright (c) <year> NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 
 import os
 import urllib.request
--- scripts/sklearn_RF.py
+++ scripts/sklearn_RF.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) <year> NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
